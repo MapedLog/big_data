52565569eee1dc9f87cf50c4733b68f6f97258ca
MapedLog <iarteta1@gmail.com>
2020-11-28 21:27:28 +0100

    Updated repo for full automatic process

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.7
+
+COPY requirements.txt /
+
+RUN pip install -r requirements.txt
+
+# Add requirements, code
+COPY big_data/resources/web/ /big_data/resources/web/
+COPY big_data/models/ /big_data/models/
+ENV PROJECT_HOME=/big_data
+# Declare and expose service listening port
+EXPOSE 5000/tcp
+
+# Declare entrypoint of that exposed service
+CMD ["python3", "/big_data/resources/web/predict_flask.py"]
---
+++ bdfi_container_init.sh
@@ -0,0 +1,73 @@
+#!/usr/bin
+
+
+######################################## README #################################################
+: 'This scripts is made to enable an operative and distributed data science architecture.
+Before launching docker a few scripts have to be executed, so several data does not have to be download when docker is installed.
+Data predictions are also be done before docker.
+'
+#Some variables that are going to be neeeded
+#export SPARK_HOME=/opt/spark
+#export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/spark/bin:/opt/spark/sbin:/opt/spark/python
+#export PYSPARK_PYTHON=/usr/bin/python3
+#export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export PROJECT_HOME=~/big_data
+#export PYTHONPATH=/opt/spark/python/lib/py4j-0.10.7-src.zip:/opt/spark/python
+
+#source /home/euskart/.profile
+
+############################################# DATA DOWNLOAD ##########################################
+if [ -d "~/big_data"]; then 
+    echo "repository already cloned"
+else
+    echo "Downloading repository"
+    cd ~
+    git clone https://github.com/MapedLog/big_data.git
+fi
+
+if [ -d "~/big_data/data" ]; then
+    echo "Data directory already exists"
+else
+    echo "Data directory does not exists. Creating data"
+    mkdir -p ~/big_data/data
+fi
+
+if [[ -f "~/big_data/data/simple_flight_delay_features.jsonl.bz2" && -f "~/big_data/data/origin_dest_distances.jsonl" && -f "~/big_data/models/sklearn_vectorizer.pkl" && -f "~/big_data/models/sklearn_regressor.pkl"]]; then
+    echo "Data has already been download"
+else
+    echo "Downloading Data"   
+    cd ~/big_data
+    sh ./resources/download_data.sh 
+    cd ~
+fi
+trained=$(ls -la ~/big_data/models | grep -e ".*.bin" | wc -l)
+if [  $trained == 7]; then
+    echo "Model alreeady trained"
+else
+    echo "Training model"
+    cd ~/big_data
+    pip3 install requirements.txt   
+    python3.7 resources/train_spark_mllib_model.py .
+    cd ~
+fi
+
+#compile sbt
+if [-f "~/big_data/flight_prediction/target/scala-2.11/flight_prediction_2.11-0.1.jar"]
+    cd ~/big_data/flight_prediction
+    sbt compile
+    sbt run
+    cd ~
+fi
+
+######################################## docker-compose ##############################################
+## Initiate the bdfi network and all the containers
+cd ~/big_data
+docker-compose up
+
+
+
+
+
+
+
+
