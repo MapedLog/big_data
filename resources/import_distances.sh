e2b59e002797c41cb31a72e250ae24d02c74ecd1
MapedLog <iarteta1@gmail.com>
2020-11-28 21:01:27 +0100

    Updated mongoimport to external mongodb

--- resources/import_distances.sh
+++ resources/import_distances.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Import our enriched airline data as the 'airlines' collection
-mongoimport -d agile_data_science -c origin_dest_distances --file data/origin_dest_distances.jsonl
-mongo agile_data_science --eval 'db.origin_dest_distances.ensureIndex({Origin: 1, Dest: 1})'
+mongoimport --host 172.23.0.5:27017 -d agile_data_science -c origin_dest_distances --file data/origin_dest_distances.jsonl
+mongo --host 172.23.0.5:27017 agile_data_science --eval 'db.origin_dest_distances.ensureIndex({Origin: 1, Dest: 1})'
